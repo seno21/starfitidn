7c57c550acb2f600ffdcb9001a3fa435ffcecec6
seno21 <adarafaranisa443@gmail.com>
2024-09-03 21:24:50 +0700

    migration detail users

--- database/migrations/2024_08_19_150622_create_detail_user.php
+++ database/migrations/2024_08_19_150622_create_detail_user.php
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('detail_user');
+        Schema::dropIfExists('detail_users');
     }
 };
