8a7ed701ac68faf81ab0467953b1ccf523d1ba7a
wawan-setiawan19 <wawan19@upi.edu>
2025-06-12 08:28:43 +0700

    refactor route

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,41 @@ Route::middleware(IsAdmin::class)->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/list-akun', [UserController::class, 'index'])->name('list-akun');
     Route::post('/ganti-role', [UserController::class, 'update'])->name('ganti-role');
+
+    /*
+    ---------------------------------------------------------------------------
+    *  Route untuk Profile Management
+    ---------------------------------------------------------------------------
+     */
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('event')->name('event.')->group(function () {
+        Route::resource('eom', EventController::class);
+        Route::get('eom/peserta/{id}', [EventController::class, 'peserta'])->name('eom.peserta');
+        Route::get('eom/kategori/{id}', [EventController::class, 'kategori'])->name('eom.kategori');
+
+        Route::post('tiket', [EventController::class, 'insertTiket'])->name('eom.insertTiket');
+        Route::post('tiketDel/{id}', [EventController::class, 'removeTiket'])->name('eom.removeTiket');
+        Route::put('updateTiket/{id}', [EventController::class, 'updateTiket'])->name('eom.updateTiket');
+
+        Route::post('kategori', [EventController::class, 'insertKategori'])->name('eom.insertKategori');
+        Route::post('kategoriDel/{id}', [EventController::class, 'removeKategori'])->name('eom.removeKategori');
+        Route::put('updateKategori/{id}', [EventController::class, 'updateKategori'])->name('eom.updateKategori');
+        Route::post('del/{id}', [EventController::class, 'remove'])->name('eom.remove');
+    });
+
+
+    Route::prefix('profit')->name('profit.')->group(function () {
+        Route::get('profit/', [ProfitController::class, 'index'])->name('index');
+    });
+
+    // Ini contoh jika middleware nya dalam bentuk array
+    Route::prefix('gallery')->name('gallery.')->group(function () {
+        Route::resource('img', GalleryController::class);
+        Route::get('download/{id}', [GalleryController::class, 'downloadImg'])->name('img.download');
+    });
 });
 
 Route::middleware(IsUser::class)->group(function () {
@@ -40,40 +75,13 @@ Route::middleware(IsUser::class)->group(function () {
     Route::post('/cancel-pesanan', [TransaksiTiketController::class, 'cancel'])->name('cancel-pesanan');
     Route::post('/checkout', [TransaksiTiketController::class, 'checkout'])->name('checkout');
 });
+
+/*
+---------------------------------------------------------------------------
+*  Route untuk WebHook Payment Gateway
+---------------------------------------------------------------------------
+ */
 Route::post('/paidInvoice', [TransaksiTiketController::class, 'paidInvoice'])->name('paidInvoice')->withoutMiddleware(VerifyCsrfToken::class);
-
-Route::middleware(IsAdmin::class)->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-// Route untuke Event Organizer Management
-Route::middleware(IsAdmin::class)->prefix('event')->name('event.')->group(function () {
-    Route::resource('eom', EventController::class);
-    Route::get('eom/peserta/{id}', [EventController::class, 'peserta'])->name('eom.peserta');
-    Route::get('eom/kategori/{id}', [EventController::class, 'kategori'])->name('eom.kategori');
-
-    Route::post('tiket', [EventController::class, 'insertTiket'])->name('eom.insertTiket');
-    Route::post('tiketDel/{id}', [EventController::class, 'removeTiket'])->name('eom.removeTiket');
-    Route::put('updateTiket/{id}', [EventController::class, 'updateTiket'])->name('eom.updateTiket');
-
-    Route::post('kategori', [EventController::class, 'insertKategori'])->name('eom.insertKategori');
-    Route::post('kategoriDel/{id}', [EventController::class, 'removeKategori'])->name('eom.removeKategori');
-    Route::put('updateKategori/{id}', [EventController::class, 'updateKategori'])->name('eom.updateKategori');
-    Route::post('del/{id}', [EventController::class, 'remove'])->name('eom.remove');
-});
-
-Route::middleware(IsAdmin::class)->prefix('profit')->name('profit.')->group(function () {
-    Route::get('profit/', [ProfitController::class, 'index'])->name('index');
-});
-
-
-// Ini contoh jika middleware nya dalam bentuk array
-Route::middleware([IsAdmin::class])->prefix('gallery')->name('gallery.')->group(function () {
-    Route::resource('img', GalleryController::class);
-    Route::get('download/{id}', [GalleryController::class, 'downloadImg'])->name('img.download');
-});
 
 Route::get('/symlink', function () {
     Artisan::call('storage:link');
